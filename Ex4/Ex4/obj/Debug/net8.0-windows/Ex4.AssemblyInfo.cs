ed6d73eba23af4127c1a09ee4490d7eff18d96c1
KsFomina <sloveckija@gmail.com>
2024-05-27 14:57:42 +0300

    +файл

--- Ex4/Ex4/obj/Debug/net8.0-windows/Ex4.AssemblyInfo.cs
+++ Ex4/Ex4/obj/Debug/net8.0-windows/Ex4.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ex4")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c3e0cbec513504a6dd729edc4aed2e51b3b0d30f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ex4")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ex4")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
